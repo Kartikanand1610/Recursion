929888570f35e17a8fdd33854ea3b3fa510025a4
kartik <kartik_anand@epam.com>
2025-04-16 22:38:03 +0530

    comments added for understanding

--- src/main/java/org/recursion/fibonacci/Fibonacci.java
+++ src/main/java/org/recursion/fibonacci/Fibonacci.java
@@ -5,6 +5,7 @@ public class Fibonacci {
         if(n<=1){
             return n;
         }
+        //by using multiple recursion calls we want for any n element we need f(n-1)+f(n-2)
         int last=getFibo(n-1);
         int slast=getFibo(n-2);
         return last+slast;
--- src/main/java/org/recursion/fibonacci/Main.java
+++ src/main/java/org/recursion/fibonacci/Main.java
@@ -3,6 +3,7 @@ package org.recursion.fibonacci;
 public class Main {
     public static void main(String[] args ){
         int n=4;
+        //calling the fibonacci function by making object of fibonacci class.
         Fibonacci fibonacci=new Fibonacci();
         int result=fibonacci.getFibo(n);
         System.out.println(result);
